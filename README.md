66d78c91c565e2fa3a526fce1dda3a6616b166ee
兮尘 <761516186@qq.com>
2020-10-22 12:40:15 +0800

    是

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # RotatEreplaceTransE
 
 
-#### 属性嵌入模块
+## 属性嵌入模块
 
 合并KG1和KG2成一个KG，对应AT={AT1, AT2}
 
@@ -11,7 +11,7 @@
 
 评分函数：A=PA+KA=参数共享模块分+属性嵌入模块分
 
-##### 参数共享模块
+### 参数共享模块
 
 因为对齐的实体在不同KG中有相同含义，也就是有相同的嵌入表示。
 
@@ -21,7 +21,7 @@
 
 这很简单，不会添加变量和变量漂移，也不会引入嵌入错误。
 
-##### 属性嵌入模块
+### 属性嵌入模块
 
 使用TransE嵌入属性三元组AT到语义向量空间。
 
@@ -32,3 +32,47 @@ energy函数E(e,a,v)=|| e+a-v ||
 用基于margin的评分函数训练。
 
 最终从TransE输出的嵌入中获得所有实体的属性嵌入AE
+
+## 数据集说明
+
+DBP15k(JAPE) 0.3
+
+## RDGCN
+
+Source code and datasets for IJCAI 2019 paper: ***[Relation-Aware Entity Alignment for Heterogeneous Knowledge Graphs](https://arxiv.org/pdf/1908.08210.pdf)***.
+
+Initial datasets are from [GCN-Align](https://github.com/1049451037/GCN-Align) and [JAPE](https://github.com/nju-websoft/JAPE).
+
+### Dependencies
+
+* Python>=3.5
+* Tensorflow>=1.8.0
+* Scipy>=1.1.0
+* Numpy
+
+### Datasets
+
+Please first download the datasets [here](http://59.108.48.35/data.tar.gz) and extract them into `data/` directory.
+
+There are three cross-lingual datasets in this folder:
+- fr-en
+- ja-en
+- zh-en
+
+Take the dataset DBP15K (ZH-EN) as an example, the folder "zh_en" contains:
+* ent_ids_1: ids for entities in source KG (ZH);
+* ent_ids_2: ids for entities in target KG (EN);
+* ref_ent_ids: entity links encoded by ids;
+* triples_1: relation triples encoded by ids in source KG (ZH);
+* triples_2: relation triples encoded by ids in target KG (EN);
+* zh_vectorList.json: the input entity feature matrix initialized by word vectors;
+
+### Running
+
+* Modify language or some other settings in *include/Config.py*
+* cd to the directory of *main.py*
+* run *main.py*
+
+> Due to the instability of embedding-based methods, it is acceptable that the results fluctuate a little bit (±1%) when running code repeatedly.
+
+> If you have any questions about reproduction, please feel free to email to wyting@pku.edu.cn.
--- att2id.py
+++ att2id.py
@@ -1,11 +1,11 @@
 class DataPreprocessor:
     types = ['fr_en', 'ja_en', 'zh_en']
     sourceKG = {
-        'before': ['atts_1', 'ent_ids_1'],
+        'before': ['attrs_1', 'ent_ids_1'],
         'after':  ['att_triple_1', 'att2id_1', 'att_val2id_1']
     }
     targetKG = {
-        'before': ['atts_2', 'ent_ids_2'],
+        'before': ['attrs_2', 'ent_ids_2'],
         'after':  ['att_triple_2', 'att2id_2', 'att_val2id_2']
     }
     after_all = ['att_triple_all', 'att2id_all', 'att_value2id_all']
@@ -13,53 +13,22 @@ class DataPreprocessor:
     att2id = {}
     att_val2id = {}
     att_triple2id = []
-
-    def read(self, triple_datapath='./data/ja_en/atts_2', ent2id_datapath='./data/ja_en/ent_ids_2'):
-        # 实体、属性值、属性
-        with open(ent2id_datapath, 'r') as r:
-            ent2ids = r.readlines()
-        for i in ent2ids:
-            ent_id, ent = i.strip().split('\t', 1)
-            self.ent2id[ent] = ent_id
-        with open(triple_datapath, 'r') as r:
-            lines = r.readlines()
-        att_id = 5882
-        att_val_id = 138814
-        for line in lines:
-            line = line.strip()
-            line = line.replace('<', '')
-            line = line.replace('>', '')
-            ent, att, att_val = line.split(' ', 2) # 实体 属性名 属性值
-            if att not in self.att2id.keys():
-                self.att2id[att] = att_id
-                att_id += 1
-            if att_val not in self.att_val2id.keys():
-                self.att_val2id[att_val] = att_val_id
-                att_val_id += 1
-            single_triple_id = []
-            single_triple_id.append(str(self.ent2id[ent]))
-            single_triple_id.append(str(self.att_val2id[att_val]))
-            single_triple_id.append(str(self.att2id[att]))
-            d = '\t'.join(single_triple_id)
-            self.att_triple2id.append(d)
-
-    def save_att_triple(self, filepath='./data/ja_en/att_triple_2'):
-        with open(filepath, 'w') as w:
-            for i in self.att_triple2id:
-                w.write(i+'\n')
-
-    def save_att2id(self, filepath='./data/ja_en/att2id_2'):
-        with open(filepath, 'w') as w:
-            for i in self.att2id:
-                w.write(str(self.att2id[i])+'\t'+i+'\n')
-
-    def save_att_val2id(self, filepath='./data/ja_en/att_val2id_2'):
-        with open(filepath, 'w') as w:
-            for i in self.att_val2id:
-                w.write(str(self.att_val2id[i])+'\t'+i+'\n')
+    att_id = 0  # default if not exist
+    att_val_id = 0  # default if not exist
 
     def getFile(self, types, filename):
         return './data/' + types + '/' + filename
+
+    def reset(self):
+        self.ent2id = {}
+        self.att2id = {}
+        self.att_val2id = {}
+        self.att_triple2id = []
+
+    def process(self):
+        self.transform()
+        self.merge_all()
+        return self
 
     def transform(self):
         KGS = [self.sourceKG, self.targetKG]
@@ -70,20 +39,63 @@ class DataPreprocessor:
                 # './data/ja_en/atts_2', './data/ja_en/ent_ids_2'
 
                 after = kg['after']
-                self.save_att_triple(after[0])
-                self.save_att2id(after[1])
-                self.save_att_val2id(after[2])
+                self.save_att_triple(self.getFile(t, after[0]))
+                self.save_att2id(self.getFile(t, after[1]))
+                self.save_att_val2id(self.getFile(t, after[2]))
+                self.reset()
+            self.att_id = 0  # default if not exist
+            self.att_val_id = 0
 
-    def merge(self, a, b, c):
-        # a+b -> c
-        # ent_ids_1 + ent_ids_2 -> ent_ids_all
-        with open(c, 'w') as f_all:
-            with open(a, 'r') as f_1:
-                f_all.writelines(f_1.readlines())
-            with open(b, 'r') as f_2:
-                f_all.writelines(f_2.readlines())
+    def read(self, triple_datapath='./data/ja_en/atts_2', ent2id_datapath='./data/ja_en/ent_ids_2'):
+        # 实体、属性值、属性
+        with open(ent2id_datapath, 'r') as r:
+            ent2ids = r.readlines()
+            print(ent2id_datapath, "\t", len(ent2ids))
+        for i in ent2ids:
+            ent_id, ent = i.strip().split('\t', 1)
+            self.ent2id[ent] = ent_id
 
-    def merge(self):
+        with open(triple_datapath, 'r') as r:
+            lines = r.readlines()
+            print(triple_datapath, "\t", len(lines))
+
+        for line in lines:
+            line = line.strip()
+            line = line.replace('<', '')
+            line = line.replace('>', '')
+            ent, att, att_val = line.split(' ', 2)  # 实体 属性名 属性值
+            if att not in self.att2id.keys():
+                self.att2id[att] = self.att_id
+                self.att_id += 1
+            if att_val not in self.att_val2id.keys():
+                self.att_val2id[att_val] = self.att_val_id
+                self.att_val_id += 1
+            single_triple_id = []
+            single_triple_id.append(str(self.ent2id[ent]))
+            single_triple_id.append(str(self.att_val2id[att_val]))
+            single_triple_id.append(str(self.att2id[att]))
+            d = '\t'.join(single_triple_id)
+            self.att_triple2id.append(d)
+
+    def save_att_triple(self, filepath='./data/ja_en/att_triple_2'):
+        print(filepath, "\t", len(self.att_triple2id))
+        with open(filepath, 'w') as w:
+            for i in self.att_triple2id:
+                w.write(i+'\n')
+
+    def save_att2id(self, filepath='./data/ja_en/att2id_2'):
+        print(filepath, "\t", len(self.att2id))
+        with open(filepath, 'w') as w:
+            for i in self.att2id:
+                w.write(str(self.att2id[i])+'\t'+i+'\n')
+
+    def save_att_val2id(self, filepath='./data/ja_en/att_val2id_2'):
+        print(filepath, "\t", len(self.att_val2id))
+        with open(filepath, 'w') as w:
+            for i in self.att_val2id:
+                w.write(str(self.att_val2id[i])+'\t'+i+'\n')
+
+    def merge_all(self):
         source_after = self.sourceKG['after']
         target_after = self.targetKG['after']
         for t in self.types:
@@ -97,9 +109,37 @@ class DataPreprocessor:
             c = self.getFile(t, 'ent_ids_all')
             self.merge(a, b, c)
 
-    def process(self):
-        self.transform()
-        self.merge()
+    def merge(self, a, b, c):
+        # a+b -> c
+        # ent_ids_1 + ent_ids_2 -> ent_ids_all
+        with open(c, 'w') as f_all:
+            with open(a, 'r') as f_1:
+                f_all.writelines(f_1.readlines())
+            with open(b, 'r') as f_2:
+                f_all.writelines(f_2.readlines())
+
+    def report(self):
+        print(self.types)
+        source_after = self.sourceKG['after']
+        target_after = self.targetKG['after']
+        for t in self.types:
+            for i in range(3):
+                a = self.getFile(t, source_after[i])
+                b = self.getFile(t, target_after[i])
+                c = self.getFile(t, self.after_all[i])
+                self.report_lines(a)
+                self.report_lines(b)
+                self.report_lines(c)
+            a = self.getFile(t, 'ent_ids_1')
+            b = self.getFile(t, 'ent_ids_2')
+            c = self.getFile(t, 'ent_ids_all')
+            self.report_lines(a)
+            self.report_lines(b)
+            self.report_lines(c)
+
+    def report_lines(self, path):
+        with open(path, "r") as f:
+            print(path, "\t\t", len(f.readlines()))
 
 
 
